2caf293b1eb54a35a389563c38fec3f52ff265b4
Francois Leduc-Primeau <francoislp@gmail.com>
2026-02-11 12:18:34 -0500

    Added tweaked arch config files for the timeloop tutorial

--- A2/install_timeloop/install_tl_step6.sh
+++ A2/install_timeloop/install_tl_step6.sh
@@ -1,8 +1,9 @@
 cd .. # i.e. ~/timeloop-accelergy
 git clone git@github.com:Accelergy-Project/timeloop-accelergy-exercises.git
 # Replace some arch config files
-EXERCISE_DIR="timeloop-accelergy-exercises/workspace/tutorial_exercises/01_accelergy_timeloop_2020_ispass/timeloop"
-cp -v ~/install_tl/3level.arch.yaml $EXERCISE_DIR/03-model-conv1d+oc-3level/arch/
-cp -v ~/install_tl/3level.arch.yaml $EXERCISE_DIR/05-mapper-conv1d+oc-3level/arch/
-cp -v ~/install_tl/3levelspatial.arch.yaml $EXERCISE_DIR/04-model-conv1d+oc-3levelspatial/arch/
-cp -v ~/install_tl/eyeriss_like.yaml $EXERCISE_DIR/06-mapper-convlayer-eyeriss/arch/
+EXERCISE_SRC="~/install_tl/tutorial_cfg"
+EXERCISE_DEST="timeloop-accelergy-exercises/workspace/tutorial_exercises/01_accelergy_timeloop_2020_ispass/timeloop"
+cp -v $EXERCISE_SRC/3level.arch.yaml $EXERCISE_DEST/03-model-conv1d+oc-3level/arch/
+cp -v $EXERCISE_SRC/3level.arch.yaml $EXERCISE_DEST/05-mapper-conv1d+oc-3level/arch/
+cp -v $EXERCISE_SRC/3levelspatial.arch.yaml $EXERCISE_DEST/04-model-conv1d+oc-3levelspatial/arch/
+cp -v $EXERCISE_SRC/eyeriss_like.yaml $EXERCISE_DEST/06-mapper-convlayer-eyeriss/arch/
